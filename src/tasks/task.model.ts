6c594c766ff6270ac5cf6c6e5a8e2a76522a680d
Asad-Ishak <asad.ishak@tillpayments.com>
2021-07-09 23:48:35 +1000

    Beginning of tutorial video

---
+++ src/tasks/task.model.ts
@@ -0,0 +1,25 @@
+export interface Task {
+  temperature?: number;
+
+  salinity?: number;
+
+  ammonia?: number;
+
+  nitrite?: number;
+
+  nitrate?: number;
+
+  phosphate?: number;
+
+  alkalinity?: number;
+
+  calcium?: number;
+
+  magnesium?: number;
+  status: TaskStatus;
+}
+
+export enum TaskStatus {
+  SUBMITTED = 'SUMBITTED',
+  NOT_SUBMITTED = 'NOT_SUBMITTED',
+}
